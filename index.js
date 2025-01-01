a78464b6d2e286f09e8c5c947e3baf67cf4e612d
BartJSDev <bartdemeyere.adegem@gmail.com>
2025-01-01 11:59:03 +0100

    Adding mouse events with jquery syntax
    Update the functions javascript file

--- index.js
+++ index.js
@@ -9,3 +9,42 @@ $("canvas").css("height", 100 + "%")
 $("body").css("backgroundColor", "black")
 $("body").css("margin", 0)
 $("body").css("overflow", "hidden")
+
+let mousedown = false;
+let prevmouseX = undefined
+let prevmouseY = undefined
+
+$("body").on("mousemove", (e) => {
+
+    if (mousedown) {
+
+        let mouseX = e.clientX * devicePixelRatio
+        let mouseY = e.clientY * devicePixelRatio
+
+        let d = dist(mouseX, mouseY, prevmouseX, prevmouseY)
+        let strokeWidth = d / 10
+
+        c.strokeStyle = "white"
+        c.lineWidth = strokeWidth
+        c.moveTo(prevmouseX, prevmouseY)
+        c.lineTo(mouseX, mouseY)
+        c.stroke()
+
+
+        prevmouseX = e.clientX * devicePixelRatio
+        prevmouseY = e.clientY * devicePixelRatio
+
+    }
+})
+
+$("body").on("mousedown", (e) => {
+
+    mousedown = true
+    prevmouseX = e.clientX * devicePixelRatio
+    prevmouseY = e.clientY * devicePixelRatio
+})
+
+$("body").on("mouseup", (e) => {
+
+    mousedown = false
+})
